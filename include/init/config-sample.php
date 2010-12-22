3745cfbe745ef44f53e5005262ae7616ce6141c0
Mathieu Despont <mathieu@martouf.ch>
2010-12-22 18:20:17 +0100

    add config sample

---
+++ include/init/config-sample.php
@@ -0,0 +1,23 @@
+<?php
+//**********************************************************
+// Database connection settings
+//**********************************************************
+$DBHost= 'localhost';
+$DBUser= 'root';
+$DBPwd = '';
+$DBName= 'yopyop';
+
+//**********************************************************
+// Local settings
+//**********************************************************
+
+global $googleMapsKey;
+
+//$googleMapsKey = "ABQIAAAAsyuA7bSIBFzUGUhfQJbrIRRXei5AM1mHoFPtGIH-DAFRL_BIIhQRx2aJ0K5GDB44ShMhg8XqdysUpQ";  // ecodev.ch
+$googleMapsKey = "ABQIAAAAsyuA7bSIBFzUGUhfQJbrIRSgySnZCpt_ZDIBPsG-wFu5a0oU6BSi8TdLywJh5fSfPHkead_uQZ4lSQ";  //yopyop.ch
+//$googleMapsKey = "ABQIAAAAsyuA7bSIBFzUGUhfQJbrIRRSMAugEfcv_MMHwK_fk7DalYdq0xT3z4lmg7IJaljNWbgDs1DlyX9MgA";  // martouf.ch
+//$googleMapsKey = "ABQIAAAAsyuA7bSIBFzUGUhfQJbrIRToDFy_CfwiqU-h2cB1nNY3fiLA3xRdox0xI5V4o-w9oEv54-aaGr5nyg"; // ou-est-la-girafe.ch
+//$googleMapsKey = "ABQIAAAAsyuA7bSIBFzUGUhfQJbrIRRy64MGyowFDpaFJTB3c2JhdnHFKhTPMrcJQk0SIeJg2wEqC6jxUpspeQ"; // koudou.ch
+
+
+?>
